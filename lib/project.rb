a166e852b2b716c4bb2e73843acc15318115bd76
Maika Kailani <queenmab051@gmail.com>
2020-04-06 00:16:11 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -14,6 +14,8 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    backer.backed_projects(self) 
+    if ! backer.backed_projects.include?(self) 
+      backer.backed_projects(self)
+    end
   end
 end
